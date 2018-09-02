18cfe60d694fb982220f83407a176a69b621bc39
Mahmoud Khaled <mahmoud.k.nasr@gmail.com>
2018-09-02 16:42:16 +0200

    Reimplement the Serial protocol and the proper logging protocol

--- SEV_WiFi_Communication.ino
+++ SEV_WiFi_Communication.ino
@@ -3,23 +3,9 @@
 #include "Tests.hpp"
 
 int clients_count_g = 0;
-const int SERIAL_WIFI_DATA_BUFFER_MAX_SIZE_C = 100;
-unsigned char serial_wifi_data_buffer_g[SERIAL_WIFI_DATA_BUFFER_MAX_SIZE_C];
-int serial_wifi_data_buffer_current_size_g = 0;
 
-void broadcast_data_buffer(){
-  send_packet(broadcast_IP_g, BROADCAST_PORT_C, serial_wifi_data_buffer_g, serial_wifi_data_buffer_current_size_g);
-}
-
-bool add_byte_to_data_buffer (byte new_byte){
-  serial_wifi_data_buffer_g[serial_wifi_data_buffer_current_size_g] = new_byte;
-  serial_wifi_data_buffer_current_size_g++;
-  if( serial_wifi_data_buffer_current_size_g == SERIAL_WIFI_DATA_BUFFER_MAX_SIZE_C){
-    serial_wifi_data_buffer_current_size_g = 0;
-    return true;
-  }else{
-    return false;
-  }
+void broadcast_data_buffer(unsigned char* data_buffer, int data_buffer_size){
+  send_packet(broadcast_IP_g, BROADCAST_PORT_C, data_buffer, data_buffer_size);
 }
 
 void setup() {
@@ -29,8 +15,7 @@ void setup() {
 }
 
 // TODO add support for deep sleep
-void loop() {
-  //log_loop();
+void loop() {  
   int result = receive_handshake_packet();
 
   if (clients_count_g == 0 && result == 1){
@@ -41,13 +26,11 @@ void loop() {
   clients_count_g += result;
   
   if (clients_count_g > 0){
-    byte b = receive_serial_byte();
-    bool is_full = add_byte_to_data_buffer(b);
-    if (is_full){
-      end_serial();
-      broadcast_data_buffer();
-      start_serial();
-    }
+    unsigned char* data_buffer;
+    int data_buffer_size;
+    receive_serial_packet(data_buffer, data_buffer_size);
+    // TODO check if u need to stop the serial here
+    broadcast_data_buffer(data_buffer, data_buffer_size);
   }
 }
 
--- Serial_Interface.hpp
+++ Serial_Interface.hpp
@@ -1,26 +1,32 @@
 #pragma once
+#include <SoftwareSerial.h>
 
-const int AT_BAUD_RATE_C = 115200;
-const int LOG_BAUD_RATE_C = 115200;
+const int AT_BAUD_RATE_C = 9600;
+const int LOG_BAUD_RATE_C = 9600;
 
 const char STARTING_SEQUENCE_C = 'a';
 const char ENDING_SEQUENCE_C = 'b';
 
-const bool LOGGING_C = false;
+const bool LOGGING_C = true;
 const bool DEBUG_C = false;
-const bool DEV_C = true;
+const bool DEV_C = false;
+
+#define RX D0
+#define TX D1
+
+SoftwareSerial log_serial(D0, D1);
 
 // Logging serial functions
 void init_logger(){
   if (LOGGING_C){
-    Serial1.begin(LOG_BAUD_RATE_C);
-    Serial1.write("start loggin");
+    log_serial.begin(LOG_BAUD_RATE_C);
+    log_serial.write("start logging");
   } 
 }
 
 void log_message(String s){
   if (LOGGING_C){
-    Serial1.write(s.c_str());
+    log_serial.write(s.c_str());
   } else if (DEV_C){
     Serial.write(s.c_str());
   }
@@ -41,8 +47,13 @@ void end_serial(){
   Serial.write(ENDING_SEQUENCE_C);
 }
 
-unsigned char receive_serial_byte(){
-  // TODO is this right i don't wait for anything here ????
-  return Serial.read();
+void receive_serial_packet(unsigned char* data_buffer, int data_buffer_size){
+  const int SERIAL_WIFI_DATA_BUFFER_MAX_SIZE_C = 100;
+  int received_data_size = Serial.readBytes(data_buffer, SERIAL_WIFI_DATA_BUFFER_MAX_SIZE_C);
+  if (received_data_size != SERIAL_WIFI_DATA_BUFFER_MAX_SIZE_C){
+    log_message(String("ERROR in receiving serial data frame: Size mismatch \n"));
+  }
+  // Take the size from the last byte of the buffer
+  data_buffer_size = (int)data_buffer[SERIAL_WIFI_DATA_BUFFER_MAX_SIZE_C - 1];
 }
 
